691b87d21fe288217d8c45efd2e40dbc5983314a
agu egbeago <georgefredrick8@gmail.com>
2022-02-07 11:34:09 +0100

    first ximmit

---
+++ module.js
@@ -0,0 +1,8 @@
+const names = require('./name')
+const sayHi = require('./utils')
+const data = require('./alternative-flavour')
+require('./mind-granade')
+
+sayHi('susan')
+sayHi(names.john)
+sayHi(names.peter)
